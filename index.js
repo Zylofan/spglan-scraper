0e070d34622580ae9c468a91018de00db73f4ed5
jtodd-work <jtodd.home@gmail.com>
2025-11-02 14:11:51 -0500

    Scrapes multiple games, but not all of them.

--- index.js
+++ index.js
@@ -14,45 +14,35 @@ const getGames = async () => {
     waitUntil: "load",
   });
 
-  await page.waitForSelector('._1fs09382p._1dzxdjxc._1dzxdj13q._1dzxdj1a4._1dzxdj1gi._1dzxdj1tp._1dzxdj2qr._1dzxdj6vy._1fs093841._1fs093848._1fs09383n._1dzxdj6tv._1dzxdj6tz._1dzxdj6ta', { 
+  await page.waitForSelector('p._1fs09382p._1dzxdjxc._1dzxdj13q._1dzxdj1a4._1dzxdj1gi._1dzxdj1tp._1dzxdj2qk._1fs093810._1dzxdj6vy._1fs093843._1fs093848._1fs09383n._1dzxdj6tv._1dzxdj6tz._1dzxdj6ti', { 
     visible: true,
     timeout: 5000 
   });
 
   console.log("Page has fully loaded!")
 
+
   const games = await page.evaluate(() => {
 
-    //const firstGame = document.querySelector("div._1sn9nye8._1dzxdj5._1dzxdj3c._1dzxdj72k._1dzxdj1tk");
-    // Fetch the sub-elements from the previously fetched quote element
-    // Get the displayed text and return it (`.innerText`)
+
     const page = document.querySelector("div._1sn9nye8._1dzxdj5._1dzxdj3c._1dzxdj72k._1dzxdj7f1._1dzxdj1tk._1dzxdj1oa")
-    const box = page.querySelector("div")
-    const game = box.querySelector("div._1sn9nye8._1dzxdj5._1dzxdj3c._1dzxdj72k._1dzxdj1tk")
-    const title = game.querySelector("p._1fs09382p._1dzxdjxc._1dzxdj13q._1dzxdj1a4._1dzxdj1gi._1dzxdj1tp._1dzxdj2qk._1fs093810._1dzxdj6vy._1fs093843._1fs093848._1fs09383n._1dzxdj6tv._1dzxdj6tz._1dzxdj6ti").innerText;
-    //const gm = firstGame.querySelector("p._1fs09382p._1dzxdjxc._1dzxdj13q._1dzxdj1a4._1dzxdj1gi._1dzxdj1tp._1dzxdj2qy._1fs0938z._1dzxdj6vy._1fs093841._1fs093848._1fs09383n._1dzxdj6tv._1dzxdj6u2._1dzxdj6t9").innerText;  
-
-    return { title, };
-
-
-    /*
-    const gameList = document.querySelectorAll("div._1sn9nye8._1dzxdj5._1dzxdj3c._1dzxdj72k._1dzxdj1tk");
+    const games = page.querySelectorAll("a > div._1sn9nye8._1dzxdj5._1dzxdj4pc._1dzxdj6cr._1dzxdj6c9._1dzxdj6ce._1dzxdj1wm._1dzxdj3c._1dzxdj6cp._1dzxdj72k._1dzxdj1tk._1dzxdj6y9._1dzxdj1v3")
 
     // Convert the quoteList to an iterable array
     // For each quote fetch the text and author
-    return Array.from(gameList).map((game) => {
+    return Array.from(games).map((game) => {
       // Fetch the sub-elements from the previously fetched quote element
       // Get the displayed text and return it (`.innerText`)
-      const title = quote.querySelector("p._1fs09382p._1dzxdjxc._1dzxdj13q._1dzxdj1a4._1dzxdj1gi._1dzxdj1tp._1dzxdj2qk._1fs093810._1dzxdj6vy._1fs093843._1fs093848._1fs09383n._1dzxdj6tv._1dzxdj6tz._1dzxdj6ti").innerText;
+      const title = game.querySelector("p._1fs09382p._1dzxdjxc._1dzxdj13q._1dzxdj1a4._1dzxdj1gi._1dzxdj1tp._1dzxdj2qk._1fs093810._1dzxdj6vy._1fs093843._1fs093848._1fs09383n._1dzxdj6tv._1dzxdj6tz._1dzxdj6ti").innerText;
       //const author = quote.querySelector(".author").innerText;
-
       return { title, };
-    });*/
+    });
   
 });
 
-  // Display the quotes
-  console.log("First game: ", games);
+console.log("Games: ", games, " ", games.length)
+
+  //console.log("There are ", count, " games.");
 
   // Close the browser
   //await browser.close();
